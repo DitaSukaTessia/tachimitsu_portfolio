a32c1670f17436a1b6ca0d6b40dbbd3e03a04593
Dita Putra Arrosyid <atashitachi434@gmail.com>
2025-10-05 21:53:56 +0700

    improving project page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: 'oklch(0.75 0.183 55.934)',
+        fontFamily: {
+          lexend: ["Lexend", "sans-serif"],
+        },
+        primary: "oklch(0.75 0.183 55.934)",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
-
+};
